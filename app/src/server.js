18b84d0cb3aeb8f63ebf852025684a40aeef5215
Akshay-parashar <akshay27041995@gmail.com>
2017-06-08 01:16:58 +0530

    Added HTML Files

--- app/src/server.js
+++ app/src/server.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var app = express();
 
+var root = process.cwd();
+
 //your routes here
-app.get('/', function (req, res) {
-    res.send("Hello World!");
+app.get('/login', function (req, res) {
+    res.sendFile('html/login.html' , {root});
 });
+
+app.get('/register', function (req, res) {
+    res.send("Register Page");
+});
+
+app.get('/home', function (req, res) {
+    res.send("Stillshare home page");
+});
+
+
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
