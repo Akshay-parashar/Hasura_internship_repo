aa4b7966ee1eb97060f2e8d07e0ada11202e7809
Akshay-parashar <akshay27041995@gmail.com>
2017-06-08 17:16:44 +0530

    latest changes

--- app/src/routes/users.js
+++ app/src/routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -30,13 +31,39 @@ router.post('/register',function(req,res,next){
 
   req.getValidationResult().then(function(result) {
       if(!result.isEmpty()) {
-          console.log("there are errors");
+          console.log("there are validation errors");
           console.log(result.isEmpty());
           var errors = result.mapped();
           res.render('register',{active_register: true, errors: errors });
       }
      else {
           console.log("no errors :)");
+          //Send an api call to hasura auth and then redirect
+          var xhr = new XMLHttpRequest();
+          var url = "http://auth.testapp.vcap.me/signup";
+          xhr.open("POST",url,true);
+          xhr.setRequestHeader("Content-type","application/json");
+          xhr.onreadystatechange = function(){
+            if(xhr.readyState == 4 && xhr.status == 200){
+              var json = JSON.parse(xhr.responseText);
+              console.log("This is the response from server" + JSON.stringify(json));
+              //alert(JSON.stringify(json.message));
+            }
+            else if(xhr.readyState ==4){
+              var json = JSON.parse(xhr.responseText);
+              console.log("Consoled Error : "+JSON.stringify(json.message));
+            }
+          }
+          var data = {};
+          data["username"] = username;
+          data["email"] = email;
+          data["password"] = password;
+          console.log(JSON.stringify(data));
+          var payload = JSON.stringify(data);
+          console.log(data);
+          console.log(jsoninsert);
+          xhr.send(jsoninsert);
+
        }
    });
 
